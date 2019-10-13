5313f8a89b036e413a7bfa63fc7ce9eec8d98965
Orhan Mekic <orhan@virtualobject.se>
2019-10-13 15:35:40 +0200

    update cusom.sh

--- custom.sh
+++ custom.sh
@@ -4,9 +4,9 @@
 HOME=/home/homestead
 
 #lets add some php debug help config:
-echo "xdebug.remote_connect_back=0" >> /etc/php/7.1/fpm/conf.d/20-xdebug.ini
-echo "xdebug.remote_autostart=1" >> /etc/php/7.1/fpm/conf.d/20-xdebug.ini
-echo "xdebug.remote_host=docker.for.win.localhost" >> /etc/php/7.1/fpm/conf.d/20-xdebug.ini
+echo "xdebug.remote_connect_back=0" >> /etc/php/7.3/fpm/conf.d/20-xdebug.ini
+echo "xdebug.remote_autostart=1" >> /etc/php/7.3/fpm/conf.d/20-xdebug.ini
+echo "xdebug.remote_host=docker.for.win.localhost" >> /etc/php/7.3/fpm/conf.d/20-xdebug.ini
 
 #let get our .vimrc (this is orhan-swe .vimrc settings)
 wget -O $HOME/.vimrc https://gist.githubusercontent.com/orhan-swe/d04d38e7d384ffe02eee2e21cc6a59a5/raw/db03ce2a97eb53c5e42ac67698b967d4569b2ac1/.vimrc
